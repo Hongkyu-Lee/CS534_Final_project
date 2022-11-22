6c34b28376ff57d59d24429f16a650aff534db9c
hongkyu <hong.kyu.lee@emory.edu>
2022-11-22 01:12:56 -0500

    encoder training added

---
+++ model/encoder.py
@@ -0,0 +1,118 @@
+import torch
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self, params):
+        super().__init__()
+
+        self.in_planes = 64
+        self.layers = []
+        self._layer_config = params['layer_config']
+        self._upsample_config = params['cnn_config']['upsample']
+        self._downsample_config = params['cnn_config']['downsample']
+        self._identity_config = params['cnn_config']['identity']
+
+        self.in_layer = self._in_block()
+
+        for c in self._layer_config:
+            self.layers.append(self._make_block(c))
+        self.layers = nn.Sequential(*self.layers)
+
+        self.out_layer = nn.Conv2d(64, 3, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        
+        y = self.in_layer(x)
+        y = self.layers(y)
+        y = self.out_layer(y)
+        y = torch.tanh(y)
+
+        return y
+
+    def _in_block(self):
+        layers = []
+        layers.append(nn.Conv2d(3, self.in_planes, kernel_size=3, stride=1, padding=1, bias=False))
+        layers.append(nn.BatchNorm2d(self.in_planes))
+        layers.append(nn.ReLU(inplace=True))
+
+        return nn.Sequential(*layers)
+    
+    def _make_block(self, factor = 0):
+        layers = []
+
+        if factor == 0: # identity
+            layers.append(nn.Conv2d(64, 64, kernel_size=self._identity_config['kernel_size'],
+                                            stride=self._identity_config['stride'],
+                                            padding=self._identity_config['padding'], bias=False))
+            layers.append(nn.BatchNorm2d(64))
+            layers.append(nn.ReLU(inplace=True))
+        elif factor < 0: # downsample
+            layers.append(nn.Conv2d(64, 64, kernel_size=self._downsample_config['kernel_size'],
+                                            stride=self._downsample_config['stride'],
+                                            padding=self._downsample_config['padding'], bias=False))
+            layers.append(nn.BatchNorm2d(64))
+            layers.append(nn.ReLU(inplace=True))
+        elif factor > 0: # upsample
+            layers.append(nn.Upsample(scale_factor=2, mode='bilinear'))
+            layers.append(nn.Conv2d(64, 64, kernel_size=self._upsample_config['kernel_size'],
+                                            stride=self._upsample_config['stride'],
+                                            padding=self._upsample_config['padding'], bias=False))
+            layers.append(nn.BatchNorm2d(64))
+            layers.append(nn.ReLU(inplace=True))
+        return nn.Sequential(*layers)
+
+
+
+CNN_PRESET = (
+    {
+        "identity" : {
+            "kernel_size" : 3,
+            "stride" : 1,
+            "padding" : 1
+        },
+        "upsample" : {
+            "kernel_size" : 3,
+            "stride" : 1,
+            "padding" : 1
+        },
+        "downsample": {
+            "kernel_size" : 3,
+            "stride" : 2,
+            "padding" : 1
+        }
+    },
+    {
+        "identity" : {
+            "kernel_size" : 5,
+            "stride" : 1,
+            "padding" : 2
+        },
+        "upsample" : {
+            "kernel_size" : 5,
+            "stride" : 1,
+            "padding" : 2
+        },
+        "downsample": {
+            "kernel_size" : 5,
+            "stride" : 2,
+            "padding" : 2
+        }  
+    },
+    {
+        "identity" : {
+            "kernel_size" : 7,
+            "stride" : 1,
+            "padding" : 3
+        },
+        "upsample" : {
+            "kernel_size" : 7,
+            "stride" : 1,
+            "padding" : 3
+        },
+        "downsample": {
+            "kernel_size" : 7,
+            "stride" : 2,
+            "padding" : 3
+        }  
+    }
+)
---
+++ train_encoder.py
@@ -0,0 +1,103 @@
+import os
+import time
+import torch
+import torch.nn as nn
+from model.encoder import Encoder
+from model.encoder import CNN_PRESET
+import torchvision
+from torch.utils.data import DataLoader
+
+from dataset.dataset import SimpleCAPTCHA
+from dataset.dataset import SIMPLECAPTCHALEN
+from dataset.dataset import ALPHANUMERIC
+
+
+
+def train_encoder(params):
+
+    encoder_save_path = os.path.join(params['encoder_save_path'], str(time.time()))
+    os.makedirs(encoder_save_path, exist_ok=True)
+
+    # cuda
+    if torch.cuda.is_available():
+        USE_CUDA = True
+    else:
+        USE_CUDA = False
+
+    _Model = Encoder(params)
+    _Loss = nn.MSELoss()
+    _Optim = torch.optim.Adam(_Model.parameters(), lr=1e-4)
+
+    if USE_CUDA:
+        _Model.cuda()
+
+    # transform
+    _transform = [torchvision.transforms.Compose([
+                        torchvision.transforms.Resize([224, 224]),
+                        torchvision.transforms.ToTensor(),
+                    ]),
+                    torchvision.transforms.Compose([
+                        torchvision.transforms.Resize([224, 224]),
+                        torchvision.transforms.ToTensor(),
+                    ]),
+                    torchvision.transforms.Compose([
+                        torchvision.transforms.Resize([224, 224]),
+                        torchvision.transforms.ToTensor(),
+                    ])]
+
+    # dataset
+    if params['dataset'] == 'CAPTCHA_SIMPLE':
+        _datapath = os.path.join(params['datapath'], params['dataset'])
+        _data = SimpleCAPTCHA((0.8, 0.1, 0.1), seed=0, path=_datapath, transform =_transform)
+    elif params['dataset'] == 'CAPTCHA_LARGE':
+        # TBA
+        exit()
+
+    _train_loader = DataLoader(_data.train, batch_size=params['batch_size'])
+    _valid_loader = DataLoader(_data.valid, batch_size=params['batch_size'])
+    _test_loader = DataLoader(_data.test, batch_size=params['batch_size'])
+
+
+    for e in range(params['epoch']):
+        _avg_loss = 0.0
+        _avg_acc = 0.0
+        for _, (_img, _label) in enumerate(_train_loader):
+            if USE_CUDA:
+                _tru = _img.clone().detach()
+                _img = _img.cuda()
+                _tru = _tru.cuda()
+            
+            _pred = _Model(_img)
+            _loss = _Loss(_pred, _tru)
+            _Optim.zero_grad()
+            _loss.backward()
+            _Optim.step()
+            _avg_loss += _loss.item()
+
+        print(f"Epoch: {e}, train loss: {_avg_loss/len(_train_loader)}")    
+
+    # save model
+    if USE_CUDA:
+        _Model.cpu()
+
+    torch.save(_Model, os.path.join(encoder_save_path, "model.pt"))
+    _Model_script = torch.jit.script(_Model)
+    _Model_script.save(os.path.join(encoder_save_path, "model_jit_scr.pt"))
+    torch.save({
+                'loss' : _avg_loss/len(_train_loader),
+                'state_dict': _Model.state_dict()
+                }, os.path.join(encoder_save_path, "model_state_dict.pt"))
+
+if __name__ == "__main__":
+
+    _params = {
+        'epoch': 10,
+        'batch_size': 64,
+        'layer_config' : [-1, -1, 0, 0, 1, 1],
+        'cnn_config' : CNN_PRESET[0],
+        'encoder_save_path' : './model/saved_encoders',
+        'datapath' : './dataset/',
+        'dataset' : 'CAPTCHA_SIMPLE'
+    }
+
+    train_encoder(_params)
